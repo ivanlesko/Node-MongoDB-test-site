264178c04b149ccda8456e333389286968b439e3
Ivan Lesko <mail@ivanlesko.com>
2014-12-10 13:43:44 -0800

    Added partials helpers to config file.

--- server/configure.js
+++ server/configure.js
@@ -15,7 +15,12 @@ module.exports = function(app) {
 	app.engine('handlebars', exphbs.create({
 		defaultLayout: 'main',
 		layoutsDir: app.get('views') + '/layouts',
-		partialsDir: [app.get('views') + '/partials']
+		partialsDir: [app.get('views') + '/partials'],
+		helpers: {
+			timeago: function(timestamp) {
+				return moment(timestamp).startOf('minute').fromNow();
+			}
+		}
 	}).engine);
 
 	app.set('view engine', 'handlebars');
