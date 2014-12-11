dc89510bce01f4e21ebb290e5f244ca68b05fb3a
Ivan Lesko <mail@ivanlesko.com>
2014-12-11 09:40:54 -0800

    Using an older version of handlebars.

--- server/configure.js
+++ server/configure.js
@@ -7,41 +7,32 @@ var path = require('path'),
    	methodOverride = require('method-override'),
    	cookieParser = require('cookie-parser'),
    	serveStatic = require('serve-static'),
-   	errorHandler = require('errorHandler');
+   	errorHandler = require('errorHandler'),
+   	moment = require('moment');
 
 module.exports = function(app) {
 	// configuration code...
 
 	app.engine('handlebars', exphbs.create({
-		defaultLayout: 'main',
-		layoutsDir: app.get('views') + '/layouts',
-		partialsDir: [app.get('views') + '/partials'],
-		helpers: {
-			timeago: function(timestamp) {
-				return moment(timestamp).startOf('minute').fromNow();
-			}
-		}
-	}).engine);
-
-	app.set('view engine', 'handlebars');
+        defaultLayout: 'main',
+        layoutsDir: app.get('views') + '/layouts',
+        partialsDir: [app.get('views') + '/partials'],
+        helpers: {
+            timeago: function(timestamp) {
+                console.log(timestamp);
+                return moment(timestamp).startOf('minute').fromNow();
+            }
+        }
+    }).engine);
+    app.set('view engine', 'handlebars');
 
 	app.use(morgan('dev'));
-
 	app.use(bodyParser({
 		uploadDir:path.join(__dirname, '../public/upload/temp')
 	}));
 
-	// app.use(bodyParser.json());
-	// app.use(bodyParser.urlencoded({
-	// 	extended: true
-	// }));
-
 	app.use(methodOverride());
-
 	app.use(cookieParser('some-secret-value-here'));
-
-	routes.initialize(app, new express.Router());
-
 	app.use('/public/', express.static(path.join(__dirname, '../public')));
 
 	if ('development' === app.get('env')) {
--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,11 @@
-var home = require('../controllers/home');
-var image = require('../controllers/image');
+var home = require('../controllers/home'),
+    image = require('../controllers/image');
 
-module.exports.initialize = function(app, router) {
-	router.get('/', home.index);
-	router.get('/images/:image_id', image.index);
-
-	router.post('/images', image.create);
-	router.post('/images/:image_id/like', image.like);
-	router.post('/images/:image_id/comment', image.comment);
-
-	app.use('/', router);
-}
+module.exports.initialize = function(app) {
+    app.get('/', home.index);
+    app.get('/images/:image_id', image.index);
+    
+    app.post('/images', image.create);
+    app.post('/images/:image_id/like', image.like);
+    app.post('/images/:image_id/comment', image.comment);
+};
